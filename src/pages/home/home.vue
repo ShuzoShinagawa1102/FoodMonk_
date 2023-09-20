df093e03de3c2b0974dd6b2248af1388c8de8221
Shuzo <cssz20040@g.nihon-u.ac.jp>
2023-09-20 18:04:47 +0900

    hi

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,48 +1,25 @@
-<!-- <template>
-  <view class="container">
-    <fmc-header />
-    <fmc-map />
-    <fmc-footer />
-  </view>
-
-</template>
-
-<script setup lang="ts">
-  import { ref } from "vue";
-</script>
-
-<style scoped>
-.container {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-}
-.map-container {
-    flex: 1;
-}
-.footer-wrapper {
-    width: 100%;
-}
-
-</style> -->
-
 <template>
-  <web-view src="../../hybrid/html/local.html"/>
+	<view>
+		<web-view :webview-styles="webviewStyles" src="/src/hybrid/html/local.html"></web-view>
+		<!-- <web-view :webview-styles="webviewStyles" src="https://isyadoko.net/doctor/map.php/30/20610618/"></web-view> -->
+		<!-- <web-view :webview-styles="webviewStyles" src="unko.html"></web-view> -->
+	</view>
 </template>
-<script setup lang="ts">
-import { ref } from 'vue'
-const vw = ref<any>(null)
-const pages = getCurrentPages()
-const postToWebview = () => {
-  const addr = {
-    lat: 36.711578,
-    lng: 119.168173
-  }
-  vw.value = pages[pages.length - 1]
-  vw.value
-    .$getAppWebview()
-    .children()[0]
-    .evalJS(`test(${JSON.stringify(addr)})`)
-}
-const message = (data: any) => {}
+
+<script>
+	export default {
+		data() {
+			return {
+				webviewStyles: {
+					progress: {
+						color: '#FF3333'
+					}
+				}
+			}
+		}
+	}
 </script>
+
+<style>
+
+</style>
